a200cd76c63ade091180358958bf9e38e53b5db3
MignonGakuba <36228508+MignonGakuba@users.noreply.github.com>
2021-12-15 15:52:02 +0100

    Create arrays.py

---
+++ arrays.py
@@ -0,0 +1,16 @@
+myLanguages = list()
+
+
+def setList(language):
+    myLanguages.append(language);
+
+
+def getList():
+    return myLanguages
+
+
+if __name__ == '__main__':
+    print([0, 0, 0][1])
+    print(["Python", "Java", "C#"][2])
+    setList("NL")
+    print(getList())
