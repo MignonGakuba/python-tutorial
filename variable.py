03003d6ba97d55a9dd2ef89899b855ab2279b6a5
MignonGakuba <36228508+MignonGakuba@users.noreply.github.com>
2021-12-15 16:03:29 +0100

    Update variable.py

--- variable.py
+++ variable.py
@@ -2,4 +2,10 @@ if __name__ == '__main__':
     greeting = "Hello World"
     word = greeting.split(" ")[0]
     print("Hello" == word)
+    # len() the methode to check the length of a variable or input
+    result = len("hello")
+    result2 = len("test")
+    print(result)
+    print(result2)
+
 
