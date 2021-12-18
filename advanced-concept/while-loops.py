8b58bdddd996cf3857882f0f12444d1e6180f98c
MignonGakuba <36228508+MignonGakuba@users.noreply.github.com>
2021-12-18 16:59:46 +0100

    Update while-loops.py
    Write a new function but in different way. Using the continue standard function

--- advanced-concept/while-loops.py
+++ advanced-concept/while-loops.py
@@ -1,23 +1,25 @@
-
 import random
 
-
-#create variables for to test the while function
+# create variables for to test the while function
 
 playerhp = 260
 enemyatkl = 60
-enemyatkh = 60
+enemyatkh = 80
 
-
-
-while playerhp > 0 :
-    dmg = random.randrange(enemyatkl,enemyatkh)
-    playerhp = playerhp -dmg
+while playerhp > 0:
+    dmg = random.randrange(enemyatkl, enemyatkh)
+    playerhp = playerhp - dmg
 
     if playerhp <= 30:
         playerhp = 30
-        print("Enemy strikes for", dmg, "points of damage" , "")
 
-    if playerhp == 30:
-        print("You have low health", playerhp, "You have been teleported to the nearest inn")
-        break
+    print("Enemy strikes for", dmg, "points of damage", "Current HP is",playerhp)
+
+    if playerhp > 30:
+        continue
+
+    print("You have low health:", playerhp, "You have been teleported to the nearest inn")
+    break
+    # if playerhp == 30:
+    #     print("You have low health: ", playerhp, "You have been teleported to the nearest inn")
+    #     break
