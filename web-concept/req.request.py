93caeef3b0dce533df358df811186d23315fdcd1
MignonGakuba <36228508+MignonGakuba@users.noreply.github.com>
2021-12-27 18:24:40 +0100

    created a html to copy the requested page into the html file

--- web-concept/req.request.py
+++ web-concept/req.request.py
@@ -1,7 +1,10 @@
 # import request to get the url request
 import requests
 
-
 r = requests.get("https://google.com")
-print("Status:",r.status_code)
+print("Status:", r.status_code)
 
+print(r.text)
+
+f = open("./page.html", "w+")
+f.write(r.text)
