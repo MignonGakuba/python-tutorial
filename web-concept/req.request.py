bdf577480242edd62fd1b6b050e4a1afb372c963
MignonGakuba <36228508+MignonGakuba@users.noreply.github.com>
2021-12-28 13:06:22 +0100

    Created a methode and learn to get variables

--- web-concept/req.request.py
+++ web-concept/req.request.py
@@ -1,7 +1,7 @@
 # import request to get the url request
 import requests
 
-r = requests.get("https://google.com")
+r = requests.get("https://bing.com")
 print("Status:", r.status_code)
 
 
@@ -11,8 +11,8 @@ f.write(r.text)
 
 
 # Creat a send to creat http request with params
-params = {"#q":"pizza"}
-r = requests.get("https://google.com",params=params)
+params = {"q":"pizza"}
+r = requests.get("https://bing.com",params=params)
 
 print("Status:", r.status_code)
 
